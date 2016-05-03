1471f07698115d885c62a2b757df70458a86a767
JoshMina <jjmoona0@gmail.com>
2016-05-04 04:34:27 +1200

    New comments, finished push, pop and print

--- clab_p1.c
+++ clab_p1.c
@@ -5,78 +5,105 @@ char data;
 struct stackNode *nextPtr;
 };
 typedef struct stackNode StackNode;
-typedef StackNode *StackNodePtr; 
+typedef StackNode *StackNodePtr; // this is the pointer in the list
 
-//Convert the infix expression to postfix notation.
+
 void convertToPostfix( char infix[], char postfix[]);
-//Determine if c is an operator.
 int isOperator(char c);
-//Determine if the precedence of operator1 is less than, equal to, or greater than
-//the precedence of operator2. The function returns -1, 0 and 1, respectively.
 int precedence(char operator1, char operator2);
-//Push a value on the stack.
 void push(StackNodePtr *topPtr, char value);
-//Pop a value off the stack.
 char pop(StackNodePtr *topPtr);
-//Return the top value of the stack without popping the stack.
 char stackTop(StackNodePtr topPtr);
-//Determine if the stack is empty.
 int isEmpty(StackNodePtr topPtr);
-//Print the stack. 
 void printStack(StackNodePtr topPtr);
 
 
 int main(void){
   int infixArray[10];
-  int 
-  console.log 
+  int postfixArray[10];
+  convertToPostfix(infixArray, postfixArray);
+ // printStack();
   return 0;
 }
-
+/*Convert the infix expression to postfix notation.*/
 void convertToPostfix( char infix[], char postfix[]){
 
 }
-
+/*Determine if c is an operator.*/
 int isOperator(char c){
-  if (c == '+' || c == '-' || c == '*' || c == '/' || c == '%'){ // c == '^' exponential is missing cuz no base operator for it
+  if (c == '+' || c == '-' || c == '*' || c == '/' || c == '%'){ // c == '^' exponential is missing because no base operator for it
     return 1;
   }
   else{
     return 0;
   }
 }
-
+/*Determine if the precedence of operator1 is less than, equal to, or greater than
+the precedence of operator2. The function returns -1, 0 and 1, respectively.*/
 int precedence(char operator1, char operator2){
-  //returns false if operator1 doesn't precede operator2
-  if ((operator1 == '-' || operator1 == '+') && (operator2 == '*' || operator2 == '/' || operator2 == '%') ){
-    return -1;
-  }
-  //returns true if operator2 precede operator1
-  else if ((operator1 == '*' || operator1 == '/' || operator1 == '%') && (operator2 == '-' || operator2 == '+')  ){
+//returns 1 if operator1 precedes operator2
+// retruns 0 if operator2 precedes operator1
+// returns 0 if both are on equal rank
+  if ((operator1 == '*' || operator1 == '/' || operator1 == '%') && (operator2 == '-' || operator2 == '+')  ){
     return 1;
   }
-  // returns 0 if both operators are equal precedence
+  else if ((operator1 == '-' || operator1 == '+') && (operator2 == '*' || operator2 == '/' || operator2 == '%') ){
+    return -1;
+  }
+  else if (operator1 == '^' && (operator2 == '-' || operator2 == '+') ){
+    return 1;
+  }
+  else if ((operator1 == '-' || operator1 == '+')  && operator2 == '^' ){
+    return -1;
+  }
+  else if (operator1 == '^' && (operator2 == '*' || operator2 == '/' || operator2 == '%') ){
+    return 1;
+  }
+  else if ((operator2 == '*' || operator2 == '/' || operator2 == '%') && operator2 == '^' ){
+    return -1;
+  }
   else {
     return 0;
   }
 }
-
+/*Push a value on the stack.*/
 void push(StackNodePtr *topPtr, char value){
-
+  struct stackNode *temp = malloc(sizeOf(StackNode));
+  if (temp == NULL){console.log("temp stackNode is NULL") }
+  temp->data = value; 
+  temp->nextPtr = topPtr; // temp is now connected to previous head
+  StackNodePtr = temp; // let the new temp be pointed
 }
-
+/*Pop a value off the stack.*/
 char pop(StackNodePtr *topPtr){
-
+  struct stackNode *temp = StackNodePtr;
+  StackNodePtr = StackNodePtr->topPtr; //Go back to previous head
+  free(temp);
+  return StackNodePtr;
 }
 
+/*Return the top value of the stack without popping the stack.*/
 char stackTop(StackNodePtr topPtr){
 
 }
-
+/*Determine if the stack is empty.*/
 int isEmpty(StackNodePtr topPtr){
 
 }
-
+/*Print the stack*/
 void printStack(StackNodePtr topPtr){
-
+  struct node *current;
+  current = topPtr;
+  if(current!= NULL){
+    printf("Stack: ");
+    do{
+      printf("%d ",current->data);
+      current = current->nextPtr;
+      }
+    while (current!= NULL);
+      printf("\n");
+    }
+    else{
+      printf("The Stack is empty\n");
+    }
 }
