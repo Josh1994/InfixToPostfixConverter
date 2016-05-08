2a2d065cd7409b00d3c807f6b40a517c7f2f497b
JoshMina <jjmoona0@gmail.com>
2016-05-09 05:34:33 +1200

    Cleaned up code, added comments and printf statements

--- clab_p1.c
+++ clab_p1.c
@@ -7,7 +7,7 @@ char data;
 struct stackNode *nextPtr;
 };
 typedef struct stackNode StackNode;
-typedef StackNode *StackNodePtr; // this is the pointer in the list
+typedef StackNode *StackNodePtr; // Pointer to StackNode
 
 
 void convertToPostfix( char infix[], char postfix[]);
@@ -23,137 +23,56 @@ void printStack(StackNodePtr topPtr);
 int main(void){
   char infixArray[50];  
   char postfixArray[50];
-  printf("Enter the inflix expression");
+  char name;
+  printf("Enter the inflix expression: ");
   fflush(stdin);
   gets(infixArray);
-
+  printf("The original expression is: ");
+  printf("%s\n",infixArray);
   convertToPostfix(infixArray, postfixArray);
+  printf("Here is the expression in postfix: \n");
   int i;
   for (i=0;i<strlen(infixArray);i++){
     printf("%c", postfixArray[i]);
   }
   return 0;
- /* int i;
-  for(i = 0; i<20;i++){
-    printf("%c", postfixArray[i]);
-  }*/
-
-
- /*StackNodePtr head = NULL;
-  int size;
-  int element = 0;
-  int counter = 0;
-
-  scanf("%d",&size);
-//  int empty = isEmpty(head);
-//  printf ("%d", empty);
-   while(counter<size){
-      printf("Enter a number to push into the stack:");
-     //
-      scanf("%d",&element);
-
-      push(&head,element); //&head is used because we are passing the pointer to stackNode's value
-     	
-      counter++;
-    }
-	printStack(head);
-  char i = pop(&head);
-  //char i = stackTop(head);
-  printStack(head);*/
+ 
 }
 /*Convert the infix expression to postfix notation.*/
 void convertToPostfix(char infix[],char postfix[]){
   StackNodePtr head = NULL;
   int i;
   int j;
-  char c;
-  //(6+2)*5-8/4
   push(&head,'(');
   printf("%d\n", strlen(infix));
   infix[strlen(infix)] = ')';
-/*  printf("%c", infix[0]);
-  printf("%c", infix[1]);
-  printf("%c\n", infix[2]);
-  printf("%c\n", postfix[0]);*/
-  //while (head != NULL){
-    for(i=0, j=0; i<= strlen(infix); i++){
-       // printf("This is the infix checker");
-       // printf("%c\n", infix[i]);fflush(stdout);
-        if (isalnum(infix[i])){
-         //printf("Entered isalnum\n");fflush(stdout);
-          postfix[j] = infix[i];
-         // printf("%c\n", infix[i]);fflush(stdout);
-         // printf("%c\n", postfix[j]);fflush(stdout);
-          j++;
-          // Working fine
-        }
-        else if (infix[i] == '('){
-         // printf("Entered 1st else\n");fflush(stdout);
-          push(&head, infix[i]);
-          printStack(head);
-          //working fine
-        }
-        else if(isOperator(infix[i])==1){
-          //printf("%c\n", infix[i]);fflush(stdout);
-         // printf("Entered 2st else\n");fflush(stdout);
-         // printf("%d\n",isOperator(stackTop(head)));fflush(stdout);
-          // printf("%c\n", stackTop(head));
-         // if(isOperator(stackTop(head)) == 1 ){
-            //int counter = 1;
-            
-            while(precedence(stackTop(head), infix[i]) == 1 || precedence(stackTop(head), infix[i]) == 0){
-             // printf("Entered 2st else prePop\n");fflush(stdout);
-             // printf("%c\n",stackTop(head));
-             // printf("%c\n", infix[i]);
-            
-              postfix[j]= pop(&head);
-              j++;
-             
-              //head->data= c;
-              //printf("%c\n",stackTop(head));
-             // printf("%c\n", infix[i]);
-             // printf("Entered 2st else pre incriment\n");fflush(stdout);
-            }
-           //printf("Entered 2st else afterwhile\n");fflush(stdout);
-          //}
-          //else{
-          //printf("Entered 2st else postLoop\n");fflush(stdout);
-         // printf("%c\n", infix[i]);
-          push(&head, infix[i]);
-          printStack(head);
-          //}
-       
-          /*if (head == NULL){
-            push(&head, infix[i]);
-          }
-          else if(head->data =='(' ){
-            push(&head, infix[i]);
-          }
-          else if (precedence(stackTop(head), infix[i]) != 1){
-            push(&head, infix[i]);
-          }
-          else{
-            postfix[j]=pop(&head);
-            j++;
-            push(&head,infix[i]);
-          }*/
-        }
-        else if(infix[i] == ')'){
-          //printf("Entered 3st else\n");fflush(stdout);
-          
-          
-          while( (stackTop(head)!= '(') && isOperator(head->data) == 1){
-            postfix[j] = pop (&head);
-            j++;
-          }
-          printStack(head);
-          pop(&head);
-          printStack(head);
-          //working fine
-        }
+  for(i=0, j=0; i<= strlen(infix); i++){
+    if (isalnum(infix[i])){
+      postfix[j] = infix[i];
+      j++;
     }
- //}
-  printf("Outside");fflush(stdout);
+    else if (infix[i] == '('){
+      push(&head, infix[i]);
+      printStack(head);
+    }
+    else if(isOperator(infix[i])==1){
+      while(precedence(stackTop(head), infix[i]) == 1 || precedence(stackTop(head), infix[i]) == 0){
+        postfix[j]= pop(&head);
+        j++;
+      }
+      push(&head, infix[i]);
+      printStack(head);
+    }
+    else if(infix[i] == ')'){
+      while( (stackTop(head)!= '(') && isOperator(head->data) == 1){
+        postfix[j] = pop (&head);
+        j++;
+      }
+      printStack(head);
+      pop(&head);
+      printStack(head);
+    }
+  }
 }
 /*Determine if c is an operator.*/
 int isOperator(char c){
@@ -194,12 +113,8 @@ int precedence(char operator1, char operator2){
   }
 }
 /*Push a value on the stack.*/
-void push(StackNodePtr *topPtr, char value){
-
-  //struct stackNode temp = malloc(sizeOf(StackNode));
+void push(StackNodePtr *topPtr, char value){ //*topPtr is pointer to pointer to stackNode
  if (*topPtr == NULL){
- 	//StackNodePtr *p= malloc (sizeof(StackNode));
- 	//topPtr = p;
   *topPtr = malloc (sizeof(StackNode)); // topPtr is derefenced because StackNodePtr *topPtr is a pointer to 
                                         // the pointer to stackNode and we want to find out the value of the pointer
                                         // to stackNode only and create a memory space in it.
@@ -208,17 +123,13 @@ void push(StackNodePtr *topPtr, char value){
   }
   else{
   StackNodePtr tmp = malloc(sizeof(StackNode));
-  //topPtr = (struct StackNode *)malloc(sizeof(StackNode));
- // if (topPtr == NULL){printf("temp stackNode is NULL"); }
   tmp->data = value; 
-  //tmp->nextPtr = malloc(sizeof(StackNode)); // temp is now connected to previous head
   tmp->nextPtr = *topPtr;
-  *topPtr = tmp; // let the new temp be pointed
+  *topPtr = tmp;
   }
 }
 /*Pop a value off the stack.*/
 char pop(StackNodePtr *topPtr){
-  //StackNodePtr = StackNodePtr->topPtr; //Go back to previous head
   char c;
   if (*topPtr == NULL){
     printf("Stack is empty \n");
@@ -227,9 +138,8 @@ char pop(StackNodePtr *topPtr){
     c = (*topPtr)->data;
     StackNodePtr temp = (*topPtr)->nextPtr;
     free(*topPtr);
-    *topPtr = temp; // go back to the previous head
+    *topPtr = temp;
   }
- // printf("Hello");fflush(stdout); // Checker to see where the method breaks
   return c;
 }
 
@@ -252,7 +162,7 @@ void printStack(StackNodePtr topPtr){
   StackNodePtr current;
   current = topPtr;
   if(current == NULL){
-		printf("Empty List");  
+		printf("The stack is empty\n");  
   }
   else{
 	while(current !=NULL){
